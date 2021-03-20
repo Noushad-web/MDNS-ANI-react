3a2c915be6ecf0c1f1f2d7b9d6f5e8dca24d3340
noushad-siddiqi <noushad.siddiqi@ablore.com>
2021-03-20 17:58:15 +0530

    add the design

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './Navbar';
+import Carousel from './Carousel'
+
+function App() {
+    return (
+        <>
+            <Navbar />
+            <Carousel />
+        </>
+    )
+}
+
+export default App;
---
+++ src/Carousel.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import img1 from './images/1.jpg';
+import img2 from './images/2.jpg';
+import img3 from './images/3.jpg';
+
+export default function Carousel() {
+    return (
+        <section className="carousel-wrapper">
+            <div className="contain">
+                <div id="carouselExampleFade" className="carousel slide carousel-fade" data-bs-ride="carousel">
+                    <div className="carousel-inner">
+                        <div className="carousel-item active">
+                            <img src={img1} className="d-block w-100" alt="..." />
+                            <div className="carousel-caption d-none d-md-block">
+                                <h5>First slide label</h5>
+                                <p>Some representative placeholder content for the first slide.</p>
+                            </div>
+                        </div>
+                        <div className="carousel-item">
+                            <img src={img2} className="d-block w-100" alt="..." />
+                            <div className="carousel-caption d-none d-md-block">
+                                <h5>First slide label</h5>
+                                <p>Some representative placeholder content for the first slide.</p>
+                            </div>
+                        </div>
+                        <div className="carousel-item">
+                            <img src={img3} className="d-block w-100" alt="..." />
+                            <div className="carousel-caption d-none d-md-block">
+                                <h5>First slide label</h5>
+                                <p>Some representative placeholder content for the first slide.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+                        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span className="visually-hidden">Previous</span>
+                    </button>
+                    <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+                        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span className="visually-hidden">Next</span>
+                    </button>
+                </div>
+            </div>
+        </section>
+    )
+}
